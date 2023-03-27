59df0b5eb5f472b72d8b40085a698ca56202d9b2
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-03-27 11:25:34 +0530

    complete responsive css design

--- src/component/Message.js
+++ src/component/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img
           src="https://images.pexels.com/photos/15737940/pexels-photo-15737940.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=loa"
@@ -12,10 +12,10 @@ const Message = () => {
       </div>
       <div className="messageContent">
         <p>hello</p>
-        {/* <img
+        <img
           src="https://user-images.githubusercontent.com/99132893/227786098-2f48105b-2034-431c-96a4-065eab0f0e80.jpg"
           alt="you"
-        /> */}
+        />
       </div>
     </div>
   );
