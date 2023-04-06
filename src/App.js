877df7d30f593bf4ea9e9b8ca3e2046714c788e1
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-04-06 17:15:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   };
   return (
     <div className="App">
-      <BrowserRouter basename="/my-chat-app">
+      <BrowserRouter>
         <ToastContainer/>
         <Routes>
           <Route exact path="/">
