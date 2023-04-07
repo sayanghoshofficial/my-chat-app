9b52871f83c4c45cc18df1d3e120df3680a34ade
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-04-07 17:17:33 +0530

    add basename

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   };
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/my-chat-app">
         <ToastContainer/>
         <Routes>
           <Route exact path="/">
